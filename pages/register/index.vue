42aff7070c15f3a557ce1f74f6faeb5030ace837
omega1234az <76193935+omega1234az@users.noreply.github.com>
2024-03-11 20:16:04 +0700

    V2-index.vue

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -11,26 +11,26 @@
             <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="register">
               <div>
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ชื่อ</label>
-                <input type="text" name="name" id="name" v-model="name"
+                <input required type="text" name="name" id="name" v-model="name"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="ชื่อ" required>
               </div>
               <div>
                 <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">นามสกุล</label>
-                <input type="text" name="lastname" id="lastname" v-model="lastname"
+                <input required type="text" name="lastname" id="lastname" v-model="lastname"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="ชื่อ" required>
               </div>
               <div>
                 <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                <input type="text" name="username" id="username" v-model="username"
+                <input required type="text" name="username" id="username" v-model="username"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="username" required>
               </div>
               <div>
                 <label for="password"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" v-model="password" placeholder="••••••••"
+                <input required type="password" name="password" id="password" v-model="password" placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required>
               </div>
