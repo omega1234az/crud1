f77b859c6da0d3f0e6286767fc596124be95441b
omega1234az <76193935+omega1234az@users.noreply.github.com>
2024-03-11 14:54:42 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
      <ul class="text-2xl flex gap-3">
         <li class="bg-blue-300 p-5 "><NuxtLink to="/login">เข้าสู่ระบบ</NuxtLink></li>
         <li class="bg-blue-300 p-5 "><NuxtLink to="/register">สมัครสมาชิก</NuxtLink></li>
-        <li class="bg-blue-300 p-5 "><NuxtLink to="/admin/edit">สมัครสมาชิก</NuxtLink></li> 
+        <li class="bg-blue-300 p-5 "><NuxtLink to="/admin/edit">CRUD1 test</NuxtLink></li> 
      </ul>
     </div>
 </template>
